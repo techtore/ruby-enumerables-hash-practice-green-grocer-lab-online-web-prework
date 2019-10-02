05940fd7d6b41eca0eccf1afe1d16f2a52fb6012
Victoria Hale <vic.hale74@gmail.com>
2019-10-02 19:29:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,13 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+  cart.each do |items_array| 
+    items_array.each do |item, attribute_hash| 
+    new_cart[item] ||= attribute_hash 
+      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
+      new_cart[item][:count] = 1 
+    end
+  end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
